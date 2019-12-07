a145009269d4b62a2f78769eb1f8bf44b1a92fbb
Evan Rittenhouse <emanguy1@gmail.com>
2019-12-07 13:12:08 -0500

    Remove sample program from library package

--- src/main/kotlin/lib/day7/Part2.kt
+++ src/main/kotlin/lib/day7/Part2.kt
@@ -3,23 +3,6 @@ package io.cloudtamer.adventofcode.lib.day7
 import io.cloudtamer.adventofcode.lib.day2.IntcodeOutput
 import io.cloudtamer.adventofcode.lib.day2.processProgram
 
-fun main() {
-    val program = listOf(
-        3,26, // Input to 26
-        1001,26,-4,26, // Subtract 4 from 26
-        3,27, // Input to 27
-        1002,27,2,27, // Multiply 27 by 2
-        1,27,26,27, // Add 26 and 27
-        4,27, // Output 27
-        1001,28,-1,28, // Subtract 1 from 28
-        1005,28,6, // If 28 != 0 jump to 6
-        99,
-        // 26, 27, and 28
-        0,0,5)
-    val result = optimizeProgram(program, listOf(5, 6, 7, 8, 9), ::feedbackAmplify)
-    println(result)
-}
-
 fun feedbackAmplify(program: List<Int>, inputSet: List<Int>): Int {
     var mostRecentFinalAmpOutput = IntcodeOutput(emptyList(), emptyList(), 0, false)
     var mostRecentAmpOutput = IntcodeOutput(emptyList(), emptyList(), 0, false)
